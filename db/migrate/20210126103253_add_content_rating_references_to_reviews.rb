ec5f6b947493b7a43bc55c3862938f49649b8eb4
Josh <joshuawilliam.barnes@gmail.com>
2021-01-26 21:37:17 +1100

    new reviews migration

---
+++ db/migrate/20210126103253_add_content_rating_references_to_reviews.rb
@@ -0,0 +1,7 @@
+class AddContentRatingReferencesToReviews < ActiveRecord::Migration[6.0]
+  def change
+    add_column :reviews, :content, :text
+    add_column :reviews, :rating, :integer
+    add_reference :reviews, :cocktail, null: false, foreign_key: true
+  end
+end
